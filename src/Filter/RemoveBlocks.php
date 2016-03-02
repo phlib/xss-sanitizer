67ecf056a221bf721acb04f4bedd63efb42bf373
Martin Price <martin.price@emailcenteruk.com>
2016-03-02 10:04:31 +0000

    Add filter for removing blocks of HTML (from opening tag to closing tag)
    This will be used before EscapeTags to try to remove script tags, and any remaining will then be escaped

---
+++ src/Filter/RemoveBlocks.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Phlib\XssSanitizer\Filter;
+
+use Phlib\XssSanitizer\FilterInterface;
+
+/**
+ * Class RemoveBlocks
+ * @package Phlib\XssSanitizer\Filter
+ */
+class RemoveBlocks implements FilterInterface
+{
+    /**
+     * @var string
+     */
+    protected $searchRegex;
+
+    /**
+     * RemoveBlocks constructor
+     * @param string|string[] $tags
+     */
+    public function __construct($tags)
+    {
+        $this->searchRegex = $this->initSearchRegex($tags);
+    }
+
+    /**
+     * Filter tags out of the HTML by removing whole blocks (from opening tag to closing tag)
+     *
+     * This filter should be used in conjunction with @see \Phlib\XssSanitizer\Filter\EscapeTags to ensure that any
+     * tags which are not picked up will be escaped
+     *
+     * e.g.
+     *     <body><script type="text/javascript">alert('XSS');</script></body>
+     * becomes
+     *     <body></body>
+     *
+     * @param string $str
+     * @return string
+     */
+    public function filter($str)
+    {
+        $str = preg_replace($this->searchRegex, '', $str);
+
+        return $str;
+    }
+
+    /**
+     * Build the search regex based on the tags specified
+     *
+     * @param string|string[] $tags
+     * @return string
+     */
+    protected function initSearchRegex($tags)
+    {
+        if (is_array($tags)) {
+            $tags = '(?:' . implode('|', $tags) . ')';
+        }
+        return implode('', [
+            '#',
+                // open tag
+                '<',
+                '(', $tags, ')',
+                '([^>]*?)',
+                '>',
+                // content
+                '.*?',
+                // closing tag
+                '</',
+                '\1',
+                '([^>]*?)',
+                '(>|$)',
+            '#i',
+        ]);
+    }
+}
---
+++ tests/Filter/RemoveBlocksTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Phlib\XssSanitizer\Test\Filter;
+
+use Phlib\XssSanitizer\Filter\RemoveBlocks;
+
+/**
+ * Class RemoveBlocksTest
+ * @package Phlib\XssSanitizer\Test\Filter
+ */
+class RemoveBlocksTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @dataProvider removeBlocksDataProvider
+     * @param string $original
+     * @param string $expected
+     */
+    public function testRemoveBlocks($original, $expected)
+    {
+
+        $actual = (new RemoveBlocks('script'))->filter($original);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function removeBlocksDataProvider()
+    {
+        return [
+            ['<body><script>alert(\'XSS\');</script></body>', '<body></body>'],
+
+            /**
+             *  this shows why this filter should be used in conjunction with @see \Phlib\XssSanitizer\Filter\EscapeTags
+             */
+            ['<body><scri<script>pt>alert(\'XSS\');<scri</script>pt></body>', '<body><script></body>'],
+            ['<body><script>var x = "</script>";alert(\'XSS\');</script></body>', '<body>";alert(\'XSS\');</script></body>'],
+        ];
+    }
+
+}
