f189a90797f60341f8aade61ad9ce4360615fce9
Martin Price <martin.price@emailcenteruk.com>
2016-03-09 10:20:29 +0000

    Add an optimistic attribute search before falling back to pessimistic
    This should avoid a lot of false positives by firstly parsing the attributes string assuming it is well formed and syntactically good.
    
    If it doesn't parse correctly, then for the remaining string it will fall back to using a pessimistic approach, where it will try to find anything which looks
    like a possible attribute string

--- src/AttributeFinder.php
+++ src/AttributeFinder.php
@@ -9,9 +9,19 @@ namespace Phlib\XssSanitizer;
 class AttributeFinder
 {
     /**
+     * @var array
+     */
+    protected $attributes;
+
+    /**
      * @var string
      */
-    protected $searchRegex;
+    protected $optimisticSearchRegex;
+
+    /**
+     * @var string
+     */
+    protected $pessimisticSearchRegex;
 
     /**
      * AttributeFinder constructor
@@ -19,7 +29,9 @@ class AttributeFinder
      */
     public function __construct($attributes)
     {
-        $this->searchRegex = $this->initSearchRegex($attributes);
+        $this->attributes             = $attributes;
+        $this->optimisticSearchRegex  = $this->initOptimisticSearchRegex();
+        $this->pessimisticSearchRegex = $this->initPessimisticSearchRegex($attributes);
     }
 
     /**
@@ -42,8 +54,79 @@ class AttributeFinder
      */
     public function findAttributes($attributes, callable $callback)
     {
-        return preg_replace_callback(
-            $this->searchRegex,
+        $filtered = [];
+
+        $this->findAttributesOptimistic($attributes, $callback, $filtered);
+
+        $this->findAttributesPessimistic($attributes, $callback, $filtered);
+
+        return implode('', $filtered);
+    }
+
+    /**
+     * Find attributes hoping for well-formed and valid HTML
+     *
+     * This should prevent a certain number of false positives by nicely handling attributes which are
+     * well formed and syntactically good
+     *
+     * This allows us to ignore cases where an attribute name appears in the context of an attribute value
+     * when we know that the attribute is well formed
+     *
+     * @param string $attributes
+     * @param callable $callback
+     * @param array $filtered
+     */
+    protected function findAttributesOptimistic(&$attributes, callable $callback, array &$filtered)
+    {
+        while (preg_match($this->optimisticSearchRegex, $attributes, $matches)) {
+            $attributes = substr($attributes, strlen($matches[0]));
+            if (in_array(strtolower($matches[3]), $this->attributes)) {
+                $replacement = $callback($matches[2], $matches[5]);
+            } else {
+                $replacement = $matches[2];
+            }
+            $filtered[] = $matches[1]; // whitespace
+            $filtered[] = $replacement;
+        }
+    }
+
+    /**
+     * Build the optimistic search regex
+     *
+     * @return string
+     */
+    protected function initOptimisticSearchRegex()
+    {
+        return implode('', [
+            '#',
+                '^(\s*)',        // group 1 (whitespace)
+                '(',             // group 2 (full attribute)
+                    '([a-z]+)',  // group 3 (attribute name)
+                    '=',
+                    '(["\'])',   // group 4 (quote)
+                    '(.*?)',     // group 5 (attribute value)
+                    '\4',
+                ')',
+            '#si',
+        ]);
+    }
+
+    /**
+     * Find attributes where the attribute syntax may not be well formed
+     *
+     * This acts as a fallback when the optimistic search is not able to parse the attributes
+     *
+     * Here, we aren't too bothered about false positives; we want to make sure we catch all and any possibilities
+     * of the attribute appearing, which may include occurances within an attribute value
+     *
+     * @param string $attributes
+     * @param callable $callback
+     * @param array $filtered
+     */
+    protected function findAttributesPessimistic($attributes, callable $callback, array &$filtered)
+    {
+        $filtered[] = preg_replace_callback(
+            $this->pessimisticSearchRegex,
             function($matches) use ($callback) {
                 if (isset($matches[2]) && $matches[2]) {
                     $attributeContents = $matches[2]; // quoted contents
@@ -62,7 +145,7 @@ class AttributeFinder
      * @param string $attributes
      * @return string
      */
-    protected function initSearchRegex($attributes)
+    protected function initPessimisticSearchRegex($attributes)
     {
         if (is_array($attributes)) {
             $attributes = '(?:' . implode('|', $attributes) . ')';
--- tests/Filter/RemoveAttributesTest.php
+++ tests/Filter/RemoveAttributesTest.php
@@ -43,8 +43,14 @@ class RemoveAttributesTest extends \PHPUnit_Framework_TestCase
             // https://www.owasp.org/index.php/XSS_Filter_Evasion_Cheat_Sheet#Non-alpha-non-digit_XSS
             ['<body onload!#$%&()*~+-_.,:;?@[/|\]^`="alert(document.cookie);">', '<body >'],
 
+            // test falling back to pessimistic parser
+            ['<body invalid="something\""onload="alert(\'XSS\')" >', '<body invalid="something\"" >'],
+            ['<body invalid="something"/onload="alert(\'XSS\')" >', '<body invalid="something"/ >'],
+            ['<a invalid="something""href="onload=alert(\'XSS\');" >', '<a invalid="something""href=" >'],
+
             // valid values
             ['<a href="http://my.website/index.php?onetimekey=abc">', '<a href="http://my.website/index.php?onetimekey=abc">'],
+            ['<a href="http://my.website/index.php?onload=dostuff">', '<a href="http://my.website/index.php?onload=dostuff">'],
         ];
     }
 
